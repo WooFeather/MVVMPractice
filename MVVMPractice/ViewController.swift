c7107a1a9f9fefb96ea77f97ac77d7077203b5af
Woohyun Jo <bluewing1004@gmail.com>
2025-02-05 17:33:38 +0900

    Initial Commit

---
+++ MVVMPractice/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  MVVMPractice
+//
+//  Created by 조우현 on 2/5/25.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
